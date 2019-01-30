7ba9265b2294663605a2e3cad5ac471ab067fe62
Steve <levkoff@gmail.com>
2019-01-30 15:52:56 -0800

    first code chunk!

--- CODE 1-30-19.R
+++ CODE 1-30-19.R
@@ -1,17 +1,60 @@
 ##CODE FROM 1/30/19
 
-##comments look like this  
+##COMMENTS COME AFTER THE # SIGN AND WON'T "RUN" CODE - THEY ARE IDOL TEXT
+##TO HELP EXPLAIN WHAT IS BEING CODED.  GOOD SCRIPTING/CODING SHOULD BE WELL
+##DOCUMENTED TO EXPLAIN WHAT IS GOING ON!
 
-##installs the swirl package - only needs to be done once
-install.packages("swirl") 
+##INSTRUCTIONS FOR INSTALLING SWIRL
+##################################
+##################################
 
-##activates/points R to the swirl library of commands = nees to be done every time you start a new session if you want to use this package
-library("swirl")  
+##STEP 1:  PACKAGE/LIBRARY INSTALLATION
+install.packages('swirl')  ##this installs the package (library)
 
-##installs a particular swirl course - only needs to be done once for the particular course (in this case, R Programming E)
-install_course_github("swirldev", "R_Programming_E")  
+##STEP 2:  LOADING THE LIBRARY
+library(swirl)  ##this loads the library and needs to be done every session before calling swirl() below
 
-##ALTERNATIVELY - you can use the following command to install a course (in this case, Exploratory Data Analysis):
-swirl::install_course("R_Programming_E")
+##STEP 3.1:  INSTALLING A COURSE
+install_course('R_Programming_E')  ##this installs the course
 
-swirl()  ##runs the program "swirl" and you and the AI take it from there
+##OR ALTERNATIVELY
+
+##STEP 3.2:  INSTALLING A COURSE (ALTERNATIVE)
+swirl::install_course('R_Programming_E')  ##this installs the course
+
+##OR ALTERNATIVELY
+
+##STEP 3.3:  INSTALLING A COURSE (ALTERNATIVE)
+install_course_github("swirldev", "R_Programming_E") ##this installs the course directly from the Github
+
+##STEP 4:  OPENING SWIRL
+swirl()  ##opens the swirl course in the console window below.
+
+##STEP 5:  COMPLETE ALL 15 MODULES OF THE R_PROGRAMMING_E COURSE
+##AS YOU COMPLETE EACH MODULE
+
+##STEP 6:  AS YOU COMPLETE THE MODULES, YOU SHOULD BE SAVING ALL OF YOUR CODE
+##FOR EACH MODULE IN AN R script file (ending in the extension .R)
+##SO YOU CAN REFERENCE YOUR SAVED CODE/WORK LATER.
+
+##STEP 7:  AT THE END OF EACH MODULE FOLLOW THE INSTRUCTIONS IN THE PROMPT CAREFULLY.
+##WHEN YOU'RE INSTRUCTED TO PROVIDE AN EMAIL ADDRESS, USE ECON386SP19@gmail.com.
+
+##STEP 8:  FOLLOW THE INSTRUCTIONS PROVIDED FOR WHAT TO INCLUDE IN THE EMAIL SUBJECT.
+
+##STEP 9:  IN THE BODY OF THE EMAIL, CUT AND PASTE YOUR WORK FROM THE MODULE THAT YOU
+##SAVED IN THE R SCRIPT FILE.
+
+##STEP 10:  SEND THE EMAIL.  DEPENDING ON HOW YOUR EMAIL CLIENT IS SET UP ON YOUR MACHINE,
+##YOUR COMPUTER MAY TRY AND OPEN OUTLOOK TO SEND THE EMAIL.  IN THIS CASE
+##YOU SHOULD FOLLOW THE INSTRUCTIONS ABOVE AND SEND THE EMAIL MANUALLY WITH THE INFORMATION
+##PROVIDED AT THE END OF THE SWIRL MODULE AND FROM YOUR WORK IN THE R SCRIPT FILE.
+
+
+
+##STEP 6:  INSTALL ANOTHER COURSE IF YOU WANT TO (OPTIONAL)
+##REVERT BACK TO STEP 3.1 and insert a different course in the "  "
+##Choose from "Exploratory Data Analysis"
+##            "Getting and Cleaning Data"
+##            "Statistical Inference"
+##            "Regression Models"
